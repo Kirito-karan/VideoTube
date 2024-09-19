21c0336a367dc39708fa2449b97c42fe16971bde
Karan <badboy.kirito1@gmail.com>
2024-09-19 11:26:20 +0530

    user & video model added

--- src/index.js
+++ src/index.js
@@ -6,3 +6,12 @@ dotenv.config({
 })
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 4000, ()=>{
+        console.log(`Server is running at port: ${process.env.PORT}`);
+        
+    })
+})
+.catch((err)=>{
+    console.log("MONGO db connection failed", err);
+})
